68a16f78ed8664e573b06b64b0b4c89696b9b0fc
Alex Leung <alexleung25@outlook.com>
2022-12-22 17:18:12 -0800

    Updated README.md

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 A WIP DOM builder. 
 
 This is ESM-only.
+
+# Goals
+
+Some current goals for this utility: 
+
+1) Able to create a file directory tree structure.
+2) Able to create basic DOM structures.
---
+++ test/evadom_test.ts
@@ -0,0 +1,75 @@
+import { EvaDOMBuilderUtil } from "../src/eva-dom-builder-util.js";
+
+function Eva() {
+    const EvaDOM = new EvaDOMBuilderUtil();
+
+    //parent folder
+    EvaDOM.DOMBuilderParent(
+        'div', 
+        'id', 
+        'TopContainer',
+        document.body
+    );
+
+    //ul node
+    EvaDOM.DOMBuilderChild(
+        'ul', 
+        'class', 
+        "myUL",  
+        undefined,
+        EvaDOMBuilderUtil.parentNode,
+        1,
+        false
+    );
+
+    EvaDOM.DOMBuilderChildNoAttr(
+        'li',
+        undefined,
+        EvaDOMBuilderUtil.prevChildNode,
+        1,
+        false
+    );
+
+    EvaDOM.DOMBuilderChild(
+        'span',
+        'class',
+        "caret",
+        "Users",
+        EvaDOMBuilderUtil.prevChildNode,
+        1,
+        false
+    );
+
+    EvaDOM.DOMBuilderChildWithRef(
+        'ul',
+        'class',
+        "nested",
+        undefined,
+        document.querySelector('li') as HTMLElement,
+        1,
+        false, 
+        true
+    )
+
+    EvaDOM.DOMBuilderChildNoAttr(
+        'li',
+        "User",
+        EvaDOMBuilderUtil.prevChildNode,
+        2,
+        true
+    );
+
+    const toggle = document.getElementsByClassName("caret");
+    
+    const nested = document.querySelector('.nested') as HTMLElement;
+
+    for(let i = 0; i < toggle.length; i++) {
+      toggle[i].addEventListener('click', function(this: Element) {
+        nested.classList.toggle('active');
+        (this as Element).classList.toggle('caret-down');
+      });
+    }
+
+    console.log(EvaDOMBuilderUtil.childNodeTempRef);
+}   
+Eva();
